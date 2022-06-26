0d84bb9ea5f4685cb503b3451d0100640ebb16a6
KaisenTheGreatOne <56113977+KaisenTheGreatOne@users.noreply.github.com>
2022-06-26 16:40:44 +0200

    Fehlerbehebung

--- CarAIProject/Assets/Scripts/Umgebungserkennung.cs
+++ CarAIProject/Assets/Scripts/Umgebungserkennung.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Umgebungserkennung : MonoBehaviour
 {
     [Header("AI")]
+    [SerializeField] private LayerMask Layers;
     [SerializeField] private Transform RayOrigin;
     [SerializeField] private float distanceF;
     [SerializeField] private float distanceR;
@@ -22,7 +23,7 @@ public class Umgebungserkennung : MonoBehaviour
     {
         RaycastHit hit;
 
-        if (Physics.Raycast(RayOrigin.position, transform.forward, out hit, 1000, LayerMask.NameToLayer("CheckPoint")))
+        if (Physics.Raycast(RayOrigin.position, transform.forward, out hit, 1000, ~Layers))
         {
             distanceF = Vector3.Distance(RayOrigin.position, hit.point);
         }
@@ -37,7 +38,7 @@ public class Umgebungserkennung : MonoBehaviour
         RaycastHit hitR;
         Vector3 vectorR = Quaternion.AngleAxis(45, Vector3.up) * transform.forward;
 
-        if (Physics.Raycast(RayOrigin.position, vectorR, out hitR, 1000, LayerMask.NameToLayer("CheckPoint")))
+        if (Physics.Raycast(RayOrigin.position, vectorR, out hitR, 1000, ~Layers))
         {
             distanceR = Vector3.Distance(RayOrigin.position, hitR.point);
         }
@@ -52,7 +53,7 @@ public class Umgebungserkennung : MonoBehaviour
         RaycastHit hitL;
         Vector3 vectorL = Quaternion.AngleAxis(-45, Vector3.up) * transform.forward;
 
-        if (Physics.Raycast(RayOrigin.position, vectorL, out hitL, 1000, LayerMask.NameToLayer("CheckPoint")))
+        if (Physics.Raycast(RayOrigin.position, vectorL, out hitL, 1000, ~Layers))
         {
             distanceL = Vector3.Distance(RayOrigin.position, hitL.point);
         }
